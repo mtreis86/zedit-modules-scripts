25c3a65efa7a9213c1e831fa999b3a7dc1bae1a6
mtreis86 <mtreis86@gmail.com>
2019-02-18 21:48:25 -0500

    Upload new experiencePatcher

---
+++ experiencePatcher/index.js
@@ -0,0 +1,33 @@
+/* global ngapp, xelib */
+registerPatcher({
+    info: info,
+    gameModes: [xelib.gmTES5, xelib.gmSSE],
+    settings: {
+    },
+    execute: (patch, helpers, settings, locals) => ({
+        initialize: (patchFile, helpers, settings, locals) => {
+            locals.entries = [];
+        },
+        process: [{
+            load: () => ({
+                signature: 'RACE',
+                filter: (record) => {
+                    const health = xelib.GetValue(record, "DATA\\Starting Health");
+                    if (!health) {
+                        return false;
+                        }
+                    helpers.logMessage("Processing " + xelib.LongName(record));
+                    let entry = new String("".concat(xelib.GetValue(record, "EDID - Editor ID"), "\;",
+                                                     xelib.GetValue(record, "DATA\\Starting Health"), "\n"));
+                    locals.entries.push(entry);
+                }
+            })
+        }],
+        finalize: (patchFile, helpers, settings, locals) => {
+            let outputText = new String (locals.entries);
+            let dataPath = xelib.GetGlobal('DataPath');
+            let filePath = fh.jetpack.path(dataPath, 'SKSE', 'Plugins', 'Experience', 'Races', 'experiencePatch.csv');
+            fh.saveTextFile(filePath, outputText.replace(/\,/g, ""));
+        }
+    })
+});
